39bd3f183984fa243d3c20d741583d5f631ce1de
ghosttwigs <145803068+ghosttwigs@users.noreply.github.com>
2023-10-11 11:58:35 -0700

    Add files via upload

---
+++ project98.py
@@ -0,0 +1,25 @@
+lower_range = int(input("lower range : "))
+upper_range = int(input("upper range : "))
+
+number = int(input("number to be divided by : "))
+
+for i in range(lower_range, upper_range+1):
+    if(i % number == 0):
+        print(i)
+
+# lower no = 10
+# upper range = 30
+
+# no to be divided by = 5
+
+# you have to display all the no between 10 to 30 which are divided by 5
+
+
+
+
+
+
+
+
+
+
